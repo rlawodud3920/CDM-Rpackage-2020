6e4af22b338ad92a0335cbe657e4f7e5bc8b3f31
rlawodud3920 <68527745+rlawodud3920@users.noreply.github.com>
2021-05-14 15:15:44 +0900

    Add files via upload

---
+++ ppi30stomachcancer/inst/sql/sql_server/GetSubgroups.sql
@@ -0,0 +1,27 @@
+SELECT row_id,
+	covariate_id,
+	covariate_value
+FROM #cov_1
+
+UNION ALL
+
+SELECT row_id,
+	covariate_id,
+	covariate_value
+FROM #cov_2
+
+UNION ALL
+
+SELECT row_id,
+	covariate_id,
+	covariate_value
+FROM #cov_3
+
+UNION ALL
+
+SELECT row_id,
+	covariate_id,
+	covariate_value
+FROM #cov_4
+
+;
